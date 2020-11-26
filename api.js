1c18640828c8cb89fad1041610b8dd295591bdd8
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2020-11-26 09:59:50 +0100

    amélioration du serveur

--- api.js
+++ api.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
-id = {id: "it340-Garcia_Godoy"};
-mongoose.connect('mongodb://localhost:27017/id', {useNewUrlParser: true});
-let app = express();
-let port = 8081;
+const port = 8800;
+const app = express();
+const id = {id: "it340-Garcia_Godoy"};
+
+// Connection à la base de données
+mongoose.connect('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true})
+.then(() => console.log('Connected to MongoDB !'))
+.catch(() => console.log('Connection to MongoDB failed !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Lecture des corps de requête en json 
+app.use(bodyParser.json());
 
 app.get('/id', function(req, res){
     res.json(id);
 })
- 
-app.listen(port, () =>  { 
-    console.log('le serveur fonctionne')
-})
+
+app.listen(port, ()=> {console.log('Server listening on port '+ port);});
