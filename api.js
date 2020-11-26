13391b002eab7a2fb478919842e7cfc799bd0118
GARCIA Marc-Antoine <Marc-Antoine.Garcia@bordeaux-inp.fr>
2020-11-26 11:14:17 +0100

    Suppression mongodb

--- api.js
+++ api.js
@@ -1,18 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
+
 const bodyParser = require('body-parser');
 
 const port = 8800;
 const app = express();
 const id = {id: "it340-Garcia_Godoy"};
-
-// Connection à la base de données
-mongoose.connect('mongodb://localhost:27017', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true})
-.then(() => console.log('Connected to MongoDB !'))
-.catch(() => console.log('Connection to MongoDB failed !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
