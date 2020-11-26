d8ec9aaa8c8552342dc1fdb88809cdf9e9a1c388
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2020-11-26 11:14:54 +0100

    ignore

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const port = 8800;
 const app = express();
 const id = {id: "it340-Garcia_Godoy"};
 
+/*
 // Connection à la base de données
 mongoose.connect('mongodb://localhost:27017', {
     useNewUrlParser: true,
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-
+*/
 // Lecture des corps de requête en json 
 app.use(bodyParser.json());
 
