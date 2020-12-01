544ddd0a0d8bf18568bf64e33640f3986b3c69c3
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2020-12-01 15:20:36 +0100

    feat: intégration standardjs finalisée. fix #6.

--- bill.js
+++ bill.js
@@ -1,18 +1,17 @@
 module.exports = class Bill {
-
-    static calcBill(prices, quantities) {
-      if( prices.length != quantities.length ){
-          throw(new Error('Lists are not the same length'));
-      } else{
-          const numberOfElements = prices.length;
-          let result = 0;
-          for(let i=0;i<numberOfElements;i++){
-              if(prices[i]<0 || quantities[i]<0){
-                throw(new Error('Negative values detected'));
-              }
-              result = result + prices[i] * quantities[i];
+  static calcBill (prices, quantities) {
+    if (prices.length !== quantities.length) {
+      throw new Error('Lists are not the same length')
+    } else {
+      const numberOfElements = prices.length
+      let result = 0
+      for (let i = 0; i < numberOfElements; i++) {
+        if (prices[i] < 0 || quantities[i] < 0) {
+          throw new Error('Negative values detected')
+        }
+        result = result + prices[i] * quantities[i]
       }
-      return result;
+      return result
     }
   }
 }
