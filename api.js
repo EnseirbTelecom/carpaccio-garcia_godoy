0338ff7e97dd20f550bbd3da5f2caa5504fa8730
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2020-12-01 15:20:36 +0100

    feat: intégration standardjs finalisée. fix #6.

--- api.js
+++ api.js
@@ -1,38 +1,36 @@
 const express = require('express')
 
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 
-const Bill = require("./bill.js");
+const Bill = require('./bill.js')
 
-const port = 8800;
-const app = express();
-const id = { id: "it340-Garcia_Godoy" };
+const port = 8800
+const app = express()
+const id = { id: 'it340-Garcia_Godoy' }
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
 
-// Lecture des corps de requête en json 
-app.use(bodyParser.json());
+// Lecture des corps de requête en json
+app.use(bodyParser.json())
 
 app.get('/id', function (req, res) {
-    res.status(200).json(id);
+  res.status(200).json(id)
 })
 
 app.post('/bill', function (req, res) {
-    let total;
-    try {
-        total = Bill.calcBill(req.body.prices, req.body.quantities);
-    }
-    catch (error) {
-        console.error(error);
-        res.status(401).json(error)
-    }
-    res.status(200).json({bill: total});
-
+  let total
+  try {
+    total = Bill.calcBill(req.body.prices, req.body.quantities)
+  } catch (error) {
+    console.error(error)
+    res.status(401).json(error)
+  }
+  res.status(200).json({ bill: total })
 })
 
-app.listen(port, () => { console.log('Server listening on port ' + port); });
+app.listen(port, () => { console.log('Server listening on port ' + port) })
