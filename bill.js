05f4cb75da4238ec6fc73f59bedc480eb1232f0f
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2020-12-17 15:41:13 +0100

    fix: #8

--- bill.js
+++ bill.js
@@ -1,17 +1,34 @@
 module.exports = class Bill {
+  
   static calcBill (prices, quantities) {
-    if (prices.length !== quantities.length) {
+    if (!this.isSameLength(prices, quantities)) {
       throw new Error('Lists are not the same length')
+    } 
+    else if(this.hasNegativeValue(prices) || this.hasNegativeValue(quantities)){
+      throw new Error('Lists contain negative values')
     } else {
       const numberOfElements = prices.length
       let result = 0
       for (let i = 0; i < numberOfElements; i++) {
-        if (prices[i] < 0 || quantities[i] < 0) {
-          throw new Error('Negative values detected')
-        }
         result = result + prices[i] * quantities[i]
       }
       return result
     }
+  }
+
+  static isSameLength(tab1, tab2) {
+    return tab1.length === tab2.length
+  }
+  static hasNegativeValue(tab) {
+    let result = false
+    let index = 0
+    while(!result && index <= tab.length) {
+        if(tab[index] < 0){
+          result = true;
+        } else {
+          index++
+        }
+    }
+    return result
   }
 }
