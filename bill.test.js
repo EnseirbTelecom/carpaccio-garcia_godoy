dc6d6286df943f16af1985bdfce8ae6daf619f84
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2020-12-17 20:41:14 +0100

    try to merge with master

--- bill.test.js
+++ bill.test.js
@@ -9,17 +9,9 @@ test("Prices and quantities don't have the same length", () => {
 })
 
 test('Negative value in prices', () => {
-<<<<<<< HEAD
   expect(Bill.calcBill([-10, 20], [1, 2])).toThrowError(new Error('Lists contain negative values'))
 })
 
 test('Negative value in quantities', () => {
   expect(Bill.calcBill([10, 20], [1, -2])).toThrowError(new Error('Lists contain negative values'))
-=======
-  expect(() => { Bill.calcBill([-10, 20], [1, 2]) }).toThrowError(new Error('Lists contain negative values'))
-})
-
-test('Negative value in quantities', () => {
-  expect(() => { Bill.calcBill([10, 20], [1, -2]) }).toThrowError(new Error('Lists contain negative values'))
->>>>>>> f386f35ac9d9581c9202288e91f8250369aa62ff
 })
