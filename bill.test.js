78c984cc78ac2bc589da17598d8ccc406af8a203
GARCIA Marc-Antoine <Marc-Antoine.Garcia@bordeaux-inp.fr>
2020-12-17 20:24:30 +0100

    Fix: bill.test.js

--- bill.test.js
+++ bill.test.js
@@ -5,13 +5,13 @@ test('Prices = [10,20] and Quantities = [1,2] expected to be 50', () => {
 })
 
 test("Prices and quantities don't have the same length", () => {
-  expect(Bill.calcBill([10, 20, 30], [1, 2])).toThrowError(new Error('Lists are not the same length'))
+  expect(() => { Bill.calcBill([10, 20, 30], [1, 2]) }).toThrowError(new Error('Lists are not the same length'))
 })
 
 test('Negative value in prices', () => {
-  expect(Bill.calcBill([-10, 20], [1, 2])).toThrowError(new Error('Negative values detected'))
+  expect(() => { Bill.calcBill([-10, 20], [1, 2]) }).toThrowError(new Error('Lists contain negative values'))
 })
 
 test('Negative value in quantities', () => {
-  expect(Bill.calcBill([10, 20], [1, -2])).toThrowError(new Error('Negative values detected'))
+  expect(() => { Bill.calcBill([10, 20], [1, -2]) }).toThrowError(new Error('Lists contain negative values'))
 })
