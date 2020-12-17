0fa2ae9f096146fc3f714c8430297176d073e6ff
GARCIA Marc-Antoine <Marc-Antoine.Garcia@bordeaux-inp.fr>
2020-12-17 22:27:48 +0100

    Fix: api.test.js + creation dossiers

--- tests/bill.test.js
+++ tests/bill.test.js
@@ -1,4 +1,4 @@
-const Bill = require('./bill')
+const Bill = require('../modules/bill')
 
 test('Prices = [10,20] and Quantities = [1,2] expected to be 50', () => {
   expect(Bill.calcBill([10, 20], [1, 2])).toBe(50)
@@ -9,9 +9,9 @@ test("Prices and quantities don't have the same length", () => {
 })
 
 test('Negative value in prices', () => {
-  expect(Bill.calcBill([-10, 20], [1, 2])).toThrowError(new Error('Lists contain negative values'))
+  expect(() => { Bill.calcBill([-10, 20], [1, 2]) }).toThrowError(new Error('Lists contain negative values'))
 })
 
 test('Negative value in quantities', () => {
-  expect(Bill.calcBill([10, 20], [1, -2])).toThrowError(new Error('Lists contain negative values'))
+  expect(() => { Bill.calcBill([10, 20], [1, -2]) }).toThrowError(new Error('Lists contain negative values'))
 })
