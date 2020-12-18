3de8bd21bd99c57fe9326fa256b91208691900c0
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2020-12-18 10:39:02 +0100

    feat : tests for discount

--- modules/discount.js
+++ modules/discount.js
@@ -1,61 +1,45 @@
 module.exports = class Discount {
+  static caclDiscountedPrice (price, discount) {
+    switch (discount) {
+      case 'NO_DISCOUNT':
+        return price
+      case 'PROGRESSIVE_DISCOUNT':
+        var progressiveDiscount = this.progressive(price)
+        return Math.round(price * (1 - progressiveDiscount / 100))
+      case 'FLAT_DISCOUNT':
+        return Math.round(price * 0.7)
+      case 'FIXED_DISCOUNT':
+        var fixedDiscount = this.fixed(price)
+        return price - fixedDiscount
 
-    NO_DISCOUNT = 0
-    PROGRESSIVE_DISCOUNT
-    FLAT_DISCOUNT = 30
-    FIXED_DISCOUNT
-
-    static progressive(price) {
-        if( price >= 50000 ){
-            return 15
-        }
-        else if( price >= 10000 ) {
-            return 10
-        }
-        else if( price >= 7000 ) {
-            return 7
-        }
-        else if( price >= 5000) {
-            return 5
-        }
-        else if( price >= 1000 ) {
-            return 3
-        }
-        return 0
+      default :
+        console.log('No discount found')
     }
+  }
 
-    static fixed(price){
-        if( price >= 100 && price < 400 ){
-            return 10
-        }
-        else if( price >= 400 && price < 1000 ){
-            return 50
-        }
-        else if( price >= 1000){
-            return 200
-        }
-        return 0
+  static progressive (price) {
+    if (price >= 50000) {
+      return 15
+    } else if (price >= 10000) {
+      return 10
+    } else if (price >= 7000) {
+      return 7
+    } else if (price >= 5000) {
+      return 5
+    } else if (price >= 1000) {
+      return 3
     }
+    return 0
+  }
 
-
-    static caclDiscountedPrice(price, discount){
-
-        switch(discount){
-            case "NO_DISCOUNT":
-                return price
-            case "PROGRESSIVE_DISCOUNT":
-                const progressiveDiscount = progressive(price)
-                return price*(1-progressiveDiscount/100)
-            case "FLAT_DISCOUNT":
-                return price*0,7
-            case "FIXED_DISCOUNT":
-                const fixedDiscount = fixed(price)
-                return price-fixedDiscount
-        }
+  static fixed (price) {
+    if (price >= 100 && price < 400) {
+      return 10
+    } else if (price >= 400 && price < 1000) {
+      return 50
+    } else if (price >= 1000) {
+      return 200
     }
-
+    return 0
+  }
 }
-
-
-
-
